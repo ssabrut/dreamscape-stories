ee36dc93dcf7c534e483a70ed40ffe2e219268f2
ssabrut <michael.gunawan2002@gmail.com>
2024-06-14 09:00:24 +0700

    Define input aggregator agent on story generator crew

---
+++ src/story_generator_crew/crew.py
@@ -0,0 +1,17 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from langchain_groq import ChatGroq
+
+@CrewBase
+class StoryGeneratorCrew():
+    """ Story Generator Crew """
+
+    agents_config = 'config/agents.yaml'
+    tasks_config = 'config/tasks.yaml'
+
+    def __init__(self) -> ChatGroq:
+        self.llm = ChatGroq(temperature=0, model="mixtral-8x7b-32768")
+
+    @agent
+    def input_aggregator(self) -> Agent:
+        return Agent(config=self.agents_config['input_aggregator_agent'], llm=self.llm)
